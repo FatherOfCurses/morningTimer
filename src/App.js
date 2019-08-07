6a0ca3323c6a01abe366a27592282f4148bbb11a
FatherOfCurses <colin.principe@gmail.com>
2019-08-07 14:25:36 -0400

    Displaying two components on screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import logo from "./heroClock.png";
+import TimeForm from "./TimeForm";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Morning Clock</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Trying to get the kids out the door on time</p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <header className="App-header">
+            <h1>Morning Clock</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Trying to get the kids out the door on time</p>
+          </header>
+        </div>
+        <TimeForm />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
