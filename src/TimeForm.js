0ae1b4851931ae8abd0f82b4258a9acd18400015
FatherOfCurses <colin.principe@gmail.com>
2019-08-12 20:05:03 -0400

    Working on main time display and capture

--- src/TimeForm.js
+++ src/TimeForm.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./TimeForm.css";
+import Clock from "react-live-clock";
 
 class TimeForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      startTime: Clock.toString,
+      targetTime: "",
+      timeDifference: "45 minutes"
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,11 +23,16 @@ class TimeForm extends React.Component {
     alert("Target time set to " + this.state.value);
     event.preventDefault();
   }
-
+  // TODO: apply CSS to this section
   render() {
     return (
       <div id="form" className="body">
         <form onSubmit={this.handleSubmit}>
+          <div />
+          <p>Current Time</p>
+          <Clock />
+          <p>Target Time</p>
+          <p>{this.state.value}</p>
           <div class="formGroup">
             <label for="targettime">
               Target Time:
@@ -34,8 +44,17 @@ class TimeForm extends React.Component {
                 onChange={this.handleChange}
               />
             </label>
-            <input type="submit" value="Go!" />
+            {
+              // TODO: add handling to prevent target time from being before start time
+            }
+            <input
+              type="submit"
+              value="Go!"
+              onClick={() => (this.state.startTime = Clock.value())}
+            />
           </div>
+          <p>Time Difference</p>
+          <p>{this.state.timeDifference}</p>
         </form>
       </div>
     );
